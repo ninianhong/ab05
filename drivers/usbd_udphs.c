aac2d07ffe5307ed057a804f3b55ad856c5c244e
Leo.Ni <1148583803@qq.com>
2020-03-06 13:30:44 +0800

    edit the usb library block

--- drivers/usbd_udphs.c
+++ drivers/usbd_udphs.c
@@ -1022,7 +1022,11 @@ static uint8_t _usbd_hal_write(uint8_t ep,
 	struct _single_xfer *xfer = &endpoint->transfer.single;
 
 	/* Return if busy */
-	while (endpoint->state > USB_HAL_ENDPOINT_IDLE);
+	//while (endpoint->state > USB_HAL_ENDPOINT_IDLE);         //add by leo
+        if (endpoint->state != USB_HAL_ENDPOINT_IDLE)
+        {
+            return USBD_STATUS_LOCKED;
+        }
 
 	/* Sending state */
 	endpoint->state = USB_HAL_ENDPOINT_SENDING;
@@ -1477,7 +1481,10 @@ uint8_t usbd_hal_set_transfer_callback(uint8_t ep,
 	struct _endpoint *endpoint = &endpoints[ep];
 
 	/* Check that the endpoint is not transferring */
-	while (endpoint->state > USB_HAL_ENDPOINT_IDLE);
+	//while (endpoint->state > USB_HAL_ENDPOINT_IDLE);     //add by leo
+        if ( endpoint->state > USB_HAL_ENDPOINT_IDLE ) {
+              return USBD_STATUS_LOCKED;
+        }
 
 	USB_HAL_TRACE("sXfrCb%d ", (unsigned)ep);
 
