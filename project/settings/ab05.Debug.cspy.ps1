08a806faced823ea13f94f6736be05e0b2ceb28e
Leo.Ni <1148583803@qq.com>
2020-03-10 11:00:38 +0800

    add include files

--- project/settings/ab05.Debug.cspy.ps1
+++ project/settings/ab05.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\AB05-Test\github-atmel\ab05-usb-device\project\settings\ab05.Debug.general.xcl" --backend -f "E:\AB05-Test\github-atmel\ab05-usb-device\project\settings\ab05.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\AB05-Test\github-atmel\usb_device\ab05\project\settings\ab05.Debug.general.xcl" --backend -f "E:\AB05-Test\github-atmel\usb_device\ab05\project\settings\ab05.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\AB05-Test\github-atmel\ab05-usb-device\project\settings\ab05.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\AB05-Test\github-atmel\ab05-usb-device\project\settings\ab05.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\AB05-Test\github-atmel\usb_device\ab05\project\settings\ab05.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\AB05-Test\github-atmel\usb_device\ab05\project\settings\ab05.Debug.driver.xcl" 
 }
